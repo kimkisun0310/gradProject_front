52dda372d4b0cfaa74d13c56e43ac0a3b4894af3
xkdlaldfjtnl <kimkisun0310@gmail.com>
2022-11-07 12:15:27 +0900

    first commit

--- lib/controller/api.dart
+++ lib/controller/api.dart
@@ -1,5 +1,5 @@
 class API{
-  static const hostconnect = "http://43.201.18.2:8080";
+  static const hostconnect = "";
   static const memberconnect = "$hostconnect/members";
   static const memberlogin = "$memberconnect/login";
   static const postconnect = "$hostconnect/posts";
